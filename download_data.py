d78221bad26303c4a7d2f3989d44f7b26bc040a8
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-06 15:14:59 -0400

    feat: Improve data downloading

--- download_data.py
+++ download_data.py
@@ -1,48 +1,26 @@
 
-import pandas as pd
 import mysql.connector
+import pandas as pd
 
-def DownloadData():
+from connection import Connection
 
-    print("- Download Data")
+def DownloadData(conn, query, file):
+
+    print("- Download Data: " + file)
 
     # DB connection
-    db_host = 'localhost'
-    db_user = 'root'
-    db_password = '0UHC72zNvywZ'
-    db_name = 'catBI'
-    db = mysql.connector.connect(host=db_host, user=db_user, passwd=db_password, db=db_name)
+    db = conn.Connect_()
     cursor = db.cursor()
 
-    # Upload records to DB
     try:
         # Download records
-        cursor.execute("SELECT id, record FROM records")
+        cursor.execute(query)
         records = cursor.fetchall()
-        db.commit()
-
-        # Download transactions 3 MONTHS
-        cursor.execute("""
-            SELECT
-                t.id_record AS id_record
-                ,t.duration AS duration
-                ,t.user AS user
-                ,t.called_at AS called_at
-                ,s.status AS status
-                ,n.nir AS nir
-            FROM transactions t
-            JOIN statuses s ON s.id = t.id_status
-            JOIN nirs n ON n.id = t.id_nir
-            WHERE called_at >= NOW() - INTERVAL 3 MONTH          
-        """)
-        transactions = cursor.fetchall()
+        print(len(records))
 
         # Save to CSV
-        df = pd.DataFrame(records)
-        df.to_csv('~/records.csv', index=False)
-
-        df2 = pd.DataFrame(transactions)
-        df2.to_csv('~/transactions.csv', index=False)
+        df = pd.DataFrame(records, columns=['record', 'duration', 'extra', 'status', 'called_at'])
+        df.to_csv(file, index=False,sep="\t", header=['record', 'duration', 'extra', 'status', 'called_at'])
 
     except mysql.connector.Error as e:
         print("- Error to download data: " + e.msg)
