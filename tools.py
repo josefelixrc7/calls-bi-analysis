9ec69b6735618bd4055b16fe7c7d87c4aac4ccc9
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-06 15:15:15 -0400

    feat: Add tools

---
+++ tools.py
@@ -0,0 +1,18 @@
+
+def FormatString(string):
+    valid_characteres = "abcdefghijklmnopqrstuvwxyzABCDEF GHIJKLMNOPQRSTUVWXYZ,¡:?@*_-!1234567890"
+    new_string = ''.join(c for c in string if c in valid_characteres)
+
+    if new_string == "nan":
+        return ""
+    else:
+        return new_string
+
+def FormatInt(string):
+    valid_characteres = "-1234567890"
+    new_int = ''.join(c for c in string if c in valid_characteres)
+
+    if new_int == "nan" or new_int == "":
+        return 0
+    else:
+        return int(new_int)
