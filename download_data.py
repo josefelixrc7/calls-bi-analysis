5d9fd38da07d3db6eb5c8dbc43317a18c9f3a0f4
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-23 14:54:57 -0400

    feat: Improve DownloadData

--- download_data.py
+++ download_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from connection import Connection
 
-def DownloadData(conn, query, file):
+def DownloadData(conn, query, file, columns):
 
     print("- Download Data: " + file)
 
@@ -19,8 +19,8 @@ def DownloadData(conn, query, file):
         print(len(records))
 
         # Save to CSV
-        df = pd.DataFrame(records, columns=['record', 'duration', 'extra', 'status', 'called_at'])
-        df.to_csv(file, index=False,sep="\t", header=['record', 'duration', 'extra', 'status', 'called_at'])
+        df = pd.DataFrame(records, columns=columns)
+        df.to_csv(file, index=False,sep="\t", header=columns)
 
     except mysql.connector.Error as e:
         print("- Error to download data: " + e.msg)
