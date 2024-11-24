eb14caa976397dc57a746427b20ce813cff6a04c
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-24 14:34:44 -0400

    feat: Add more analysis

--- analysis.py
+++ analysis.py
@@ -3,14 +3,66 @@ import mysql.connector
 
 from connection import Connection
 
+# DB connection
+conn = Connection('localhost', 'root', '0UHC72zNvywZ', 'catBI')
+db = conn.Connect_()
+cursor = db.cursor()
+
+def AnalysisCentroForaneo():
+
+    print("- AnalysisCentroForaneo")
+
+    try:
+        # Truncate records_preselected
+        cursor.execute("TRUNCATE TABLE records_selected")
+        db.commit()
+
+        # Insert records
+        cursor.execute(
+        """
+            INSERT INTO records_selected(id_record, id_nir)
+            SELECT r.id, n.id
+            FROM records r
+            LEFT JOIN segments_records sr ON sr.id_record = r.id
+            JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
+            WHERE
+                sr.id_record IS NULL
+                AND n.region = 'Centro Foráneo'
+        """)
+        db.commit()
+
+    except mysql.connector.Error as e:
+        print("- Error to make AnalysisCentroForaneo: " + e.msg)
+
+def AnalysisCentroMetropolitano():
+
+    print("- AnalysisCentroMetropolitano")
+
+    try:
+        # Truncate records_preselected
+        cursor.execute("TRUNCATE TABLE records_selected")
+        db.commit()
+
+        # Insert records
+        cursor.execute(
+        """
+            INSERT INTO records_selected(id_record, id_nir)
+            SELECT r.id, n.id
+            FROM records r
+            LEFT JOIN segments_records sr ON sr.id_record = r.id
+            JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
+            WHERE
+                sr.id_record IS NULL
+                AND n.region = 'Centro Metropolitano'
+        """)
+        db.commit()
+
+    except mysql.connector.Error as e:
+        print("- Error to make AnalysisCentroMetropolitano: " + e.msg)
+
 def AnalysisNoreste():
 
     print("- AnalysisNoreste")
-
-    # DB connection
-    conn = Connection('localhost', 'root', '0UHC72zNvywZ', 'catBI')
-    db = conn.Connect_()
-    cursor = db.cursor()
 
     try:
         # Truncate records_preselected
@@ -33,3 +85,107 @@ def AnalysisNoreste():
 
     except mysql.connector.Error as e:
         print("- Error to make AnalysisNoreste: " + e.msg)
+
+def AnalysisNoroeste():
+
+    print("- AnalysisNoroeste")
+
+    try:
+        # Truncate records_preselected
+        cursor.execute("TRUNCATE TABLE records_selected")
+        db.commit()
+
+        # Insert records
+        cursor.execute(
+        """
+            INSERT INTO records_selected(id_record, id_nir)
+            SELECT r.id, n.id
+            FROM records r
+            LEFT JOIN segments_records sr ON sr.id_record = r.id
+            JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
+            WHERE
+                sr.id_record IS NULL
+                AND n.region = 'Noroeste'
+        """)
+        db.commit()
+
+    except mysql.connector.Error as e:
+        print("- Error to make AnalysisNoroeste: " + e.msg)
+
+def AnalysisOccidente():
+
+    print("- AnalysisOccidente")
+
+    try:
+        # Truncate records_preselected
+        cursor.execute("TRUNCATE TABLE records_selected")
+        db.commit()
+
+        # Insert records
+        cursor.execute(
+        """
+            INSERT INTO records_selected(id_record, id_nir)
+            SELECT r.id, n.id
+            FROM records r
+            LEFT JOIN segments_records sr ON sr.id_record = r.id
+            JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
+            WHERE
+                sr.id_record IS NULL
+                AND n.region = 'Occidente'
+        """)
+        db.commit()
+
+    except mysql.connector.Error as e:
+        print("- Error to make AnalysisOccidente: " + e.msg)
+
+def AnalysisSur():
+
+    print("- AnalysisSur")
+
+    try:
+        # Truncate records_preselected
+        cursor.execute("TRUNCATE TABLE records_selected")
+        db.commit()
+
+        # Insert records
+        cursor.execute(
+        """
+            INSERT INTO records_selected(id_record, id_nir)
+            SELECT r.id, n.id
+            FROM records r
+            LEFT JOIN segments_records sr ON sr.id_record = r.id
+            JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
+            WHERE
+                sr.id_record IS NULL
+                AND n.region = 'Sur'
+        """)
+        db.commit()
+
+    except mysql.connector.Error as e:
+        print("- Error to make AnalysisSur: " + e.msg)
+
+def AnalysisSureste():
+
+    print("- AnalysisSureste")
+
+    try:
+        # Truncate records_preselected
+        cursor.execute("TRUNCATE TABLE records_selected")
+        db.commit()
+
+        # Insert records
+        cursor.execute(
+        """
+            INSERT INTO records_selected(id_record, id_nir)
+            SELECT r.id, n.id
+            FROM records r
+            LEFT JOIN segments_records sr ON sr.id_record = r.id
+            JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
+            WHERE
+                sr.id_record IS NULL
+                AND n.region = 'Sureste'
+        """)
+        db.commit()
+
+    except mysql.connector.Error as e:
+        print("- Error to make AnalysisSureste: " + e.msg)
