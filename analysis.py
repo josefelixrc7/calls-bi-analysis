6e7ad9e77fc3afc8a679dcfbbcf4fffa9d392c39
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-23 21:45:39 -0400

    feat: Add first analysis

---
+++ analysis.py
@@ -0,0 +1,35 @@
+
+import mysql.connector
+
+from connection import Connection
+
+def AnalysisNoreste():
+
+    print("- AnalysisNoreste")
+
+    # DB connection
+    conn = Connection('localhost', 'root', '0UHC72zNvywZ', 'catBI')
+    db = conn.Connect_()
+    cursor = db.cursor()
+
+    try:
+        # Truncate records_preselected
+        cursor.execute("TRUNCATE TABLE records_preselected")
+        db.commit()
+
+        # Insert records
+        cursor.execute(
+        """
+            INSERT INTO records_preselected(id_record)
+            SELECT r.id
+            FROM records r
+            LEFT JOIN segments_records sr ON sr.id_record = r.id
+            JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
+            WHERE
+                sr.id_record IS NULL
+                AND n.region = 'Noreste'
+        """)
+        db.commit()
+
+    except mysql.connector.Error as e:
+        print("- Error to make AnalysisNoreste: " + e.msg)
