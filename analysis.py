f885a759ead4b06b750723600fdf99fffe15dd39
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-26 21:35:32 -0400

    feat: Improve analysis class

--- analysis.py
+++ analysis.py
@@ -32,14 +32,14 @@ class Analysis:
                         '52' AS phone_code
                         ,r.record AS phone_number
                         ,n.region AS region
-                        ,n.state AS estado
+                        ,n.state AS esstado
                         ,n.city AS plaza
                         ,IFNULL(JSON_EXTRACT(ri.info, '$.client'), "") AS cliente
                         ,IFNULL(JSON_EXTRACT(ri.info, '$.curp'), "") AS curp
                     FROM segments s
                     JOIN segments_records sr ON sr.id_segment = s.id 
                     JOIN records r ON r.id = sr.id_record 
-                    LEFT JOIN records_info ri ON ri.id_record = r.id 
+                    JOIN records_info ri ON ri.id_record = r.id 
                     JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
                     WHERE s.id = """ + str(it[0]) + """
                     ORDER BY RAND()
