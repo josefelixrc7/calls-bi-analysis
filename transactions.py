5a1e81d878de2c8a73fc34f14cf1946317ae2944
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-11 12:07:36 -0400

    feat: Improve transactions update last

--- transactions.py
+++ transactions.py
@@ -125,19 +125,6 @@ class Transactions:
             """)
             self.db.commit()
 
-            # Update durations
-            self.cursor.execute("""
-                UPDATE transactions_last tl
-                JOIN records r ON r.id = tl.id_record
-                JOIN transactions_pre tp ON tp.record = r.record
-                SET
-                    tl.duration = tp.duration
-                    ,tl.created_at = NOW()
-                WHERE 
-                    tp.duration > tl.duration
-            """)
-            self.db.commit()
-
             # Update called_at
             self.cursor.execute("""
                 UPDATE transactions_last tl
@@ -145,9 +132,22 @@ class Transactions:
                 JOIN transactions_pre tp ON tp.record = r.record
                 SET
                     tl.called_at = tp.called_at
-                    ,tl.created_at = NOW()
+                    ,tl.updated_at = NOW()
                 WHERE 
-                    tp.called_at > tl.called_at
+                    tp.called_at > tl.called_at OR tl.called_at IS NULL
+            """)
+            self.db.commit()
+
+            # Update durations
+            self.cursor.execute("""
+                UPDATE transactions_last tl
+                JOIN records r ON r.id = tl.id_record
+                JOIN transactions_pre tp ON tp.record = r.record
+                SET
+                    tl.duration = tp.duration
+                    ,tl.updated_at = NOW()
+                WHERE 
+                    tp.duration > tl.duration OR tl.duration IS NULL
             """)
             self.db.commit()
 
