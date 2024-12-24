14bc3dc95f066c68e0f0110bbf87396a8a1bd6ab
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-24 10:59:03 -0400

    feat: Add AnalysisMN10

--- analysis.py
+++ analysis.py
@@ -326,3 +326,61 @@ class Analysis:
 
         except mysql.connector.Error as e:
             print("- Error to make AnalysisEFE20: " + e.msg)
+
+    def AnalysisMN10(self):
+
+        print("- AnalysisMN10")
+
+        try:
+            # Truncate best nirs
+            self.cursor.execute("TRUNCATE TABLE best_nirs")
+            self.db.commit()
+
+            # Update best nirs
+            self.cursor.execute(
+            """
+                INSERT INTO best_nirs(id_nir)
+                SELECT s.id_nir
+                FROM (
+                    SELECT
+                        t.id_nir AS id_nir
+                        ,COUNT(1) AS transfered
+                        ,SUM(IF(st.sale = 1, 1, 0)) AS sales
+                    FROM transactions t
+                    JOIN statuses st ON st.id = t.id_status
+                    WHERE
+                        t.called_at >= NOW() - INTERVAL 1 MONTH
+                        AND st.contact = 1
+                    GROUP BY t.id_nir
+                ) s
+                WHERE
+                    s.transfered >= 1000
+                    AND s.sales > 0
+                ORDER BY (s.transfered / s.sales) ASC
+                LIMIT 10
+            """)
+            self.db.commit()
+
+            # Truncate records_preselected
+            self.cursor.execute("TRUNCATE TABLE records_selected")
+            self.db.commit()
+
+            # Insert records
+            self.cursor.execute(
+            """
+                INSERT INTO records_selected(id_record, id_nir)
+                SELECT r.id, n.id
+                FROM records_preselected rp
+                JOIN records r ON r.id = rp.id_record
+                LEFT JOIN segments_records sr ON sr.id_record = r.id
+                JOIN nirs n ON n.nir = SUBSTRING(r.record, 1, 3)
+                JOIN best_nirs bn ON bn.id_nir = n.id
+                WHERE
+                    sr.id_record IS NULL
+                GROUP BY r.id
+            """)
+            self.db.commit()
+            self.ViewSelectedRecords()
+
+        except mysql.connector.Error as e:
+            print("- Error to make AnalysisMN10: " + e.msg)
