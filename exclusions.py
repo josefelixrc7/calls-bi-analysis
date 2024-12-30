d4db6fd8009c53fcbff2df2d8fca1a3c8c14c6eb
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-29 20:59:29 -0400

    feat: Improve ExcludeNoDuration

--- exclusions.py
+++ exclusions.py
@@ -63,7 +63,7 @@ def ExcludeNoDuration():
                 sr.id_record IS NULL
                 AND t.called_at >= NOW() - INTERVAL 1 MONTH
             GROUP BY t.id_record
-            HAVING MAX(t.duration) = 0
+            HAVING MAX(t.duration) = 0 AND COUNT(1) >= 7
         """)
         db.commit()
 
