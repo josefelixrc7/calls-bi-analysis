6e178bae565d3a05a3faeee44f5aeb99e2bd281c
Jose F Rivas C <josefelixrc7@gmail.com>
2025-01-16 13:43:21 -0400

    feat: Improve import_db

--- import_db.py
+++ import_db.py
@@ -1,9 +1,14 @@
 # Add new DB
 import functions.records as r
 
+print("CSV File: ", end="")
+csv_file = input()
+
+print("DB Type: ", end="")
+db_type = input()
+
+print("Source: ", end="")
+source = input()
+
 new_db = r.Records()
-new_db.Add(
-    input()
-    ,input()
-    ,input()
-)
+new_db.Add(csv_file, db_type, source)
